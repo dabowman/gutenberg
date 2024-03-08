f8d7333d83c39e3bd45e024c2cad8f21864a6550
scruffian <ben@scruffian.com>
2024-03-08 17:15:25 +0000

    Show the default empty variation as well as the other presets

--- packages/edit-site/src/components/global-styles/variations/variations-typography.js
+++ packages/edit-site/src/components/global-styles/variations/variations-typography.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { useContext } from '@wordpress/element';
+import { useContext, useMemo } from '@wordpress/element';
 import {
 	__experimentalGrid as Grid,
 	__experimentalVStack as VStack,
@@ -25,44 +25,42 @@ export default function TypographyVariations() {
 	const typographyVariations =
 		useCurrentMergeThemeStyleVariationsWithUserConfig( {
 			property: 'typography',
-			filter: ( variation ) =>
-				variation?.settings?.typography?.fontFamilies &&
-				Object.keys( variation?.settings?.typography?.fontFamilies )
-					.length,
 		} );
 
 	const { base } = useContext( GlobalStylesContext );
 
-	if ( ! typographyVariations?.length ) {
-		return null;
-	}
-
 	/*
 	 * Filter duplicate variations based on the font families used in the variation.
 	 */
-	const uniqueTypographyVariations = typographyVariations?.length
-		? Object.values(
-				typographyVariations.reduce( ( acc, variation ) => {
-					const [ bodyFontFamily, headingFontFamily ] =
-						getFontFamilies(
-							mergeBaseAndUserConfigs( base, variation )
-						);
-					if (
-						headingFontFamily?.name &&
-						bodyFontFamily?.name &&
-						! acc[
-							`${ headingFontFamily?.name }:${ bodyFontFamily?.name }`
-						]
-					) {
-						acc[
-							`${ headingFontFamily?.name }:${ bodyFontFamily?.name }`
-						] = variation;
-					}
+	const uniqueTypographyVariations = useMemo( () => {
+		return typographyVariations?.length
+			? Object.values(
+					typographyVariations.reduce( ( acc, variation ) => {
+						const [ bodyFontFamily, headingFontFamily ] =
+							getFontFamilies(
+								mergeBaseAndUserConfigs( base, variation )
+							);
+						if (
+							headingFontFamily?.name &&
+							bodyFontFamily?.name &&
+							! acc[
+								`${ headingFontFamily?.name }:${ bodyFontFamily?.name }`
+							]
+						) {
+							acc[
+								`${ headingFontFamily?.name }:${ bodyFontFamily?.name }`
+							] = variation;
+						}
 
-					return acc;
-				}, {} )
-		  )
-		: [];
+						return acc;
+					}, {} )
+			  )
+			: [];
+	}, [ base, typographyVariations ] );
+
+	if ( ! typographyVariations?.length ) {
+		return null;
+	}
 
 	return (
 		<VStack spacing={ 3 }>
@@ -70,25 +68,23 @@ export default function TypographyVariations() {
 				columns={ 3 }
 				className="edit-site-global-styles-style-variations-container"
 			>
-				{ typographyVariations && typographyVariations.length
-					? uniqueTypographyVariations.map( ( variation, index ) => (
-							<Variation key={ index } variation={ variation }>
-								{ ( isFocused ) => (
-									<PreviewIframe
-										label={ variation?.title }
-										isFocused={ isFocused }
-									>
-										{ ( { key } ) => (
-											<TypographyExample
-												key={ key }
-												variation={ variation }
-											/>
-										) }
-									</PreviewIframe>
+				{ uniqueTypographyVariations.map( ( variation, index ) => (
+					<Variation key={ index } variation={ variation }>
+						{ ( isFocused ) => (
+							<PreviewIframe
+								label={ variation?.title }
+								isFocused={ isFocused }
+							>
+								{ ( { key } ) => (
+									<TypographyExample
+										key={ key }
+										variation={ variation }
+									/>
 								) }
-							</Variation>
-					  ) )
-					: null }
+							</PreviewIframe>
+						) }
+					</Variation>
+				) ) }
 			</Grid>
 		</VStack>
 	);
